00693b7d1b6eb6bd5b53c406992ceaa7012e890d
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-06-28 12:18:19 +0200

    Add collbacks error with connecton db

--- db.js
+++ db.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose')
 var url = process.env.MONGODB_URI || 'mongodb://localhost/socialLocalhost'
+
+// When successfully connected
 mongoose.connect(url, function() {
     console.log('mongodb connected')
 })
+
+// If the connection throws an error
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose default connection disconnected');
+});
+
 module.exports = mongoose
