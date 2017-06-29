ce9211ef27939dd49b23ca226ebe2d06a1680a1d
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-06-29 11:52:55 +0200

    remove console.log, add bodyparser, change headers to body

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var Post = require('./models/post')
 const path = require('path')
 var app = express()
+app.use(bodyParser.json())
 
 
 app.get('/', function(req, res) {
-    console.log()
     res.sendFile(path.join(__dirname, '/', 'layouts/posts.html'))
 })
 
 app.get('/posts', function(req, res, next) {
-    console.log("posts get")
-    Post.find(function(err, posts) {
-        console.log("posts get in:" + posts)
-        if (err) { return next(err) }
-        res.send(posts)
-    })
-    console.log("posts get end")
+    Post.find()
+        .sort('-date')
+        .exec(function(err, posts) {
+            if (err) { return next(err) }
+            res.send(posts)
+        })
 })
 
 //use postman
 app.post('/posts', function(req, res, next) {
-    console.log("posts post")
-    console.log(req.headers.username)
     var post = new Post({
-        username: req.headers.username,
-        body: req.headers.body
+        username: req.body.username,
+        body: req.body.body
     })
-
     post.save(function(err, post) {
         if (err) { return next(err) }
-        console.log("Post added")
-        res.send(201)
+        res.send(201, post)
     })
-    console.log("posts post end")
 })
 
 var port = process.env.PORT || 3002
