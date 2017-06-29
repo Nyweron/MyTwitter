09da7559bf1b3586a76231bc282eea82f41a189a
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-06-29 13:15:09 +0200

    Build router as app in controller/api/posts.js, refaktor code

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var Post = require('./models/post')
 const path = require('path')
 var app = express()
+
 app.use(bodyParser.json())
+app.use('/posts', require('./controllers/api/posts'))
 
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/', 'layouts/posts.html'))
 })
 
-app.get('/posts', function(req, res, next) {
-    Post.find()
-        .sort('-date')
-        .exec(function(err, posts) {
-            if (err) { return next(err) }
-            res.send(posts)
-        })
-})
-
-//use postman
-app.post('/posts', function(req, res, next) {
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    })
-    post.save(function(err, post) {
-        if (err) { return next(err) }
-        res.send(201, post)
-    })
-})
 
 var port = process.env.PORT || 3002
 app.listen(port, function() {
