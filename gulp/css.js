76acd3f8142275a1a331ddb3952e8dddb153004a
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-06-30 16:16:15 +0200

    Add livereload to gulp

--- gulp/css.js
+++ gulp/css.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
+var livereload = require('gulp-livereload')
 
 gulp.task('css', function() {
     return gulp.src('css/**/*.styl')
         .pipe(stylus())
         .pipe(gulp.dest('assets'))
+        .pipe(livereload())
 })
 
 gulp.task('watch:css', ['css'], function() {
--- gulp/scripts.js
+++ gulp/scripts.js
@@ -4,6 +4,7 @@ var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
 var gutil = require('gulp-util')
 var sourcemaps = require('gulp-sourcemaps')
+var livereload = require('gulp-livereload')
 
 gulp.task('js', function() {
     return gulp.src(['ng/module.js', 'ng/**/*.js'])
@@ -14,6 +15,7 @@ gulp.task('js', function() {
         .on('error', function(err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('assets'))
+        .pipe(livereload())
 })
 
 gulp.task('watch:js', ['js'], function() {
