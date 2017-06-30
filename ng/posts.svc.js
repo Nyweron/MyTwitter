4ac3b26918fe2d851f4226c6930fc3737dfe79ec
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-06-30 13:54:10 +0200

    Add table for service, thanks that gulp take all files to process of minimalization

--- ng/posts.svc.js
+++ ng/posts.svc.js
@@ -1,8 +1,9 @@
-app.service('PostsSvc', function($http) {
+app.service('PostsSvc', ['$http', function($http) {
     this.fetch = () => {
+        console.error("Problem")
         return $http.get('/posts')
     }
     this.create = (post) => {
         return $http.post('/posts', post)
     }
-})
+}])
