47688182e2137df123c7401afdb70dd0f3a6f519
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-01 22:18:33 +0200

    small changes

--- ng/register.ctrl.js
+++ ng/register.ctrl.js
@@ -1,11 +1,17 @@
 angular.module('app')
     .controller('RegisterCtrl', function($scope, UserSvc) {
         $scope.register = function(username, password) {
-            UserSvc.register(username, password)
-                .then(function(response) {
-                    $scope.$emit('register', "Konto zarejestrowane poprawnie, zaloguj się.")
-                    $scope.username = ""
-                    $scope.password = ""
-                })
+            if (username && password) {
+                console.log("test")
+                UserSvc.register(username, password)
+                    .then(function(response) {
+                        $scope.$emit('register', "Konto zarejestrowane poprawnie, zaloguj się.")
+                        $scope.username = ""
+                        $scope.password = ""
+                    })
+            } else {
+                console.log("test2")
+            }
+
         }
     })
