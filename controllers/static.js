fe8fa04d6b6e99ccf0db5a7b0be72bd4f303ca10
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-01 15:58:13 +0200

    Add route ng-Route, refactor html files

--- controllers/static.js
+++ controllers/static.js
@@ -3,9 +3,11 @@ var router = express.Router()
 const path = require('path')
 
 router.use(express.static(path.join(__dirname, './../', 'assets')))
+router.use(express.static(path.join(__dirname, './../', 'layouts')))
+router.use(express.static(path.join(__dirname, './../', 'templates')))
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './../', 'layouts/posts.html'))
+    res.sendFile(path.join(__dirname, './../', 'layouts/app.html'))
 })
 
 module.exports = router
