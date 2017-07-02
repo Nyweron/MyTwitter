df98533c3fd511f21e9bb580ea7a592812757126
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-02 11:24:59 +0200

    Small changes add valid to register

--- ng/register.ctrl.js
+++ ng/register.ctrl.js
@@ -1,6 +1,29 @@
 angular.module('app')
     .controller('RegisterCtrl', function($scope, UserSvc) {
+        $scope.formInfo = {}
         $scope.register = function(username, password) {
+
+
+            $scope.nameRequired = ''
+            $scope.emailRequired = ''
+            $scope.passwordRequired = ''
+
+            console.log("tetst")
+            console.log(username)
+            console.log(password)
+            if (!username) {
+                $scope.nameRequired = 'Name Required';
+            } else {
+                $scope.nameRequired = '';
+            }
+
+            if (!password) {
+                $scope.passwordRequired = 'Password Required';
+            } else {
+                $scope.passwordRequired = '';
+            }
+
+
             if (username && password) {
                 console.log("test")
                 UserSvc.register(username, password)
