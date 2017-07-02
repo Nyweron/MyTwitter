9949d7a82e60a6acc6ac48994445e0b8ac3bc7cb
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-02 17:47:23 +0200

    ADd info about connect new client on web

--- websockets.js
+++ websockets.js
@@ -1,7 +1,26 @@
 var ws = require("ws")
+var _ = require("lodash")
+var clients = []
+
+
+/*Add or remove clients on web*/
 exports.connect = function(server) {
     var wss = new ws.Server({ server: server })
     wss.on('connection', function(ws) {
+        clients.push(ws)
+        exports.broadcast("Connect new client")
+
+        ws.on('close', function() {
+            _.remove(clients, ws)
+        })
         ws.send('Witaj')
+    })
+}
+
+/*Send info about new post to the avaiable clients on web*/
+exports.broadcast = function(topic, data) {
+    var json = JSON.stringify({ topic: topic, data: data })
+    clients.forEach(function(clients) {
+        clients.send(json)
     })
 }
