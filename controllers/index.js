1bd2986b9c6a431973d52df6b68b79bbe05cbecb
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-06 21:13:39 +0200

    Add base router because I need this to use SuperTest

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+var router = require('express').Router()
+var bodyParser = require('body-parser')
+
+
+router.use(bodyParser.json())
+router.use(require('../auth'))
+router.use('/posts', require('../controllers/api/posts'))
+router.use('/sessions', require('../controllers/api/sessions'))
+router.use('/users', require('../controllers/api/users'))
+router.use(require('./static'))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express')
-var bodyParser = require('body-parser')
-
 var app = express()
+var websockets = require('./websockets')
 
-app.use(bodyParser.json())
-app.use(require('./auth'))
-app.use('/posts', require('./controllers/api/posts'))
-app.use(require('./controllers/static'))
-
-app.use('/sessions', require('./controllers/api/sessions'))
-app.use('/users', require('./controllers/api/users'))
-
+app.use(require('./controllers'))
 
 var port = process.env.PORT || 3002
 var server = app.listen(port, function() {
-    console.log('app listening on port', port)
+    console.log('Server', process.pid, ' listening on port', port)
 })
-require('./websockets').connect(server)
+websockets.connect(server)
