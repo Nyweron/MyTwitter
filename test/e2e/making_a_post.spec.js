66b983f99365ba310303b32a41f7bf9afad3b2fe
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-06 14:55:04 +0200

    Start write test in mocha

---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,10 @@
+describe('utworzenie postu', function() {
+    it('zalogowaniee i utworzenie nowego postu', function() {
+        //go to home page.
+        //click login.
+        //write form and send.
+        //write new mytwitt 
+
+        //user should see your mytweet 
+    })
+})
