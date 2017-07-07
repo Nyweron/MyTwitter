1ae86e38dd9fed8e837b0d1d0ad68a8c7f91274a
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-07 19:56:29 +0200

    Add start and postinstall in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
+var gulp = require('gulp')
 
 fs.readdirSync(__dirname + '/gulp/').forEach(function(task) {
     require('./gulp/' + task)
 })
+
+gulp.task('build', ['css', 'js'])
