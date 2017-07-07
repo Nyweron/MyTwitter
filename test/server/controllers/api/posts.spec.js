d7ddbfeb68b63c0079f16b8e1cd9c34f2e80c404
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-07 14:28:01 +0200

    Add test for posts with auth user

--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,17 +1,15 @@
 var expect = require('chai').expect
 var ctrl = require('../../../../controllers/api/posts')
 var api = require('../support/api')
+var user = require('../support/user')
 var Post = require('../../../../models/post')
 
-describe('controllers.api.posts', function() {
 
+describe('controllers.api.posts', function() {
     beforeEach(function(done) {
         Post.remove({}, done)
     })
-
-
-    describe('GET /api/posts', function() {
-
+    describe('GET /posts', function() {
         beforeEach(function(done) {
             var posts = [
                 { body: 'post1', username: 'dzem' },
@@ -29,6 +27,31 @@ describe('controllers.api.posts', function() {
                 })
                 .end(done)
         })
+    })
 
+    describe('POST /posts', function() {
+        var token
+
+        beforeEach(function(done) {
+            user.create('dzem', 'pass', function(err, user) {
+                token = user.token
+                done(err)
+            })
+        })
+
+        beforeEach(function(done) {
+            api.post('/posts')
+                .send({ body: 'this is my new post' })
+                .set('X-Auth', token)
+                .expect(201)
+                .end(done)
+        })
+
+        it('added 1 new post', function(done) {
+            Post.findOne(function(err, post) {
+                expect(post.body).to.equal('this is my new post')
+                done(err)
+            })
+        })
     })
 })
