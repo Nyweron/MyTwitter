002746e6076b9c8f887f8bc0849b12fff1b3acd5
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2017-07-07 14:16:25 +0200

    Check new 3 posts, test Get/posts

--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,13 +1,34 @@
 var expect = require('chai').expect
 var ctrl = require('../../../../controllers/api/posts')
 var api = require('../support/api')
+var Post = require('../../../../models/post')
 
 describe('controllers.api.posts', function() {
+
+    beforeEach(function(done) {
+        Post.remove({}, done)
+    })
+
+
     describe('GET /api/posts', function() {
-        it('exists', function(done) {
+
+        beforeEach(function(done) {
+            var posts = [
+                { body: 'post1', username: 'dzem' },
+                { body: 'post2', username: 'dzem' },
+                { body: 'post3', username: 'dzem' }
+            ]
+            Post.create(posts, done)
+        })
+
+        it('it has 3 posts', function(done) {
             api.get('/posts')
                 .expect(200)
+                .expect(function(response) {
+                    expect(response.body).to.have.length(3)
+                })
                 .end(done)
         })
+
     })
 })
